98a52d60b33649125e5ff4f89a5375721002d5d6
Jared Higlefort <jared.hilgefort@ge.com>
2017-05-19 10:35:23 -0400

    Navigation and Routing Basics
    * Configure routes in AppModule, tie to actions and place a view
    * Define base element in index.html
    * Add RouterLink directive to clickable element
    * RouterOutlet specifies where to place the view

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,19 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { ProductListComponent } from './products/product-list.component';
 import { StarComponent } from './shared/star.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule, HttpModule ],
+  imports: [ BrowserModule, FormsModule, HttpModule, RouterModule.forRoot([
+    {path: 'products', component: ProductListComponent},
+    {path: 'product/:id', component: ProductListComponent},
+    {path: 'welcome', component: WelcomeComponent},
+    {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+    {path: '**', component: PageNotFoundComponent}
+  ],
+  { useHash: true }) ],
   declarations: [
     AppComponent,
+    PageNotFoundComponent,
+    ProductDetailComponent,
     ProductListComponent,
     ProductFilterPipe,
-    StarComponent
+    StarComponent,
+    WelcomeComponent
   ],
   bootstrap: [ AppComponent ]
 })
---
+++ app/page-not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'app/page-not-found.component.html'
+})
+export class PageNotFoundComponent {
+  pageLink: string = 'NOTFOUND';
+}
